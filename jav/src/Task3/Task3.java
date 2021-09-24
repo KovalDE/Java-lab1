688c67bd737b1407900335d3309949625af790e8
KovalDE <72768859+KovalDE@users.noreply.github.com>
2021-09-24 12:20:46 +0300

    Add files via upload

--- jav/src/Task3/Task3.java
+++ jav/src/Task3/Task3.java
@@ -2,17 +2,17 @@ package Task3;
 
 public class Task3 {
     public static void main(final String[] args) {
-        final char[] ArrayChar = {'d','m','i','t','r','o'};
-        final int[] ArrayInt = {1,5,7,9,0};
-        String ArrayString  = "";
-        for(int i=0;i< Math.max(ArrayInt.length,ArrayChar.length);i++) {
-            if (i < ArrayInt.length) {
-                ArrayString += ArrayInt[i];
+        final char[] arrayChar = {'d','m','i','t','r','o'};
+        final int[] arrayInt = {1,5,7,9,0};
+        String arrayString  = "";
+        for(int i=0;i< Math.max(arrayInt.length,arrayChar.length);i++) {
+            if (i < arrayInt.length) {
+                arrayString += arrayInt[i];
             }
-            if (i<ArrayChar.length) {
-                ArrayString += ArrayChar[i];
+            if (i<arrayChar.length) {
+                arrayString += arrayChar[i];
             }
         }
-        System.out.println(ArrayString);
+        System.out.println(arrayString);
     }
 }
